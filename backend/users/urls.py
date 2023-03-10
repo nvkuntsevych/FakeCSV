19eb1ce78c1824f8f6d5c8b02e6a0c1b3fe40ead
Nazarii Kuntsevych <nazar.kuntsevych@gmail.com>
2023-03-10 19:59:25 +0200

    add functionality for user logout

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from users.views import LoginUserView
+from users.views import LoginUserView, logout_user
 
+
+app_name = 'users'
 
 urlpatterns = [
-    path('login/', LoginUserView.as_view()),
+    path('login/', LoginUserView.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
